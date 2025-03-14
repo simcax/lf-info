947ff9277395f17d70c6216cd4617923e0601387
Carsten Skov <carsten@simcax.dk>
2025-03-14 07:58:02 +0100

    Configuring sentry with environment and tracing

--- lf_info/__init__.py
+++ lf_info/__init__.py
@@ -1,13 +1,18 @@
+import os
+
 import sentry_sdk
 from flask import Flask
 
 from .routes import setup_routes
+
+app_environment = os.environ.get("ENVIRONMENT_NAME", "development")
 
 sentry_sdk.init(
     dsn="https://a788df580e7cdc6711aa4b7a6d48e786@o4505902934130688.ingest.us.sentry.io/4508949981691904",
     # Add data like request headers and IP for users,
     # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
+    environment=app_environment,
 )
 
 
@@ -17,5 +22,8 @@ def create_app():
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = "/"
     app.config["PREFERRED_URL_SCHEME"] = "https"
+    if app_environment == "development":
+        app.config["DEBUG"] = True
+        app.config["TESTING"] = True
     setup_routes(app)
     return app
