8f3cea3d3c82347201e4fd38a2f93491664ef98a
Carsten Skov <carsten@simcax.dk>
2025-03-26 12:32:58 +0100

    Implemented CORS to allow cross domain trafic

--- lf_info/__init__.py
+++ lf_info/__init__.py
@@ -2,6 +2,7 @@ import os
 
 import sentry_sdk
 from flask import Flask
+from flask_cors import CORS
 
 from .routes import setup_routes
 
@@ -16,9 +17,19 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
 )
 
+# List of allowed origins
+allowed_origins = [os.environ.get("ALLOWED_ORIGIN")]
+
+
+def cors_origin(origin):
+    if origin in allowed_origins:
+        return origin
+    return None
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=allowed_origins)
     app.config["SECRET"] = "dev"
     app.config["SERVER_NAME"] = "localhost"
     app.config["APPLICATION_ROOT"] = "/"
